ca019e42c48732b7225ddfc1f551b0f05f6045de
Marchell Kevandra <marchellkevandra@gmail.com>
2025-08-02 16:17:19 +0700

    first commit

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -34,9 +34,9 @@ const Contact = () => {
         "template_17us8im",
         {
           from_name: formData.name,
-          to_name: "Ali",
+          to_name: "Marchell Kevandra",
           from_email: formData.email,
-          to_email: "AliSanatiDev@gmail.com",
+          to_email: "marchellkevandra@gmail.com",
           message: formData.message,
         },
         "pn-Bw_mS1_QQdofuV"
