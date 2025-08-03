4cb9088f4e2695d38b9d696bd1655a80e5b6dd2b
Marchell Kevandra <117437628+mkxchl@users.noreply.github.com>
2025-08-04 00:00:28 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,20 @@ import Projects from "./sections/Projects";
 import Experiences from "./sections/Experiences";
 import Testimonial from "./sections/Testimonial";
 import Contact from "./sections/Contact";
-import Footer from './sections/Footer';
+import Footer from "./sections/Footer";
+import ScrollReveal from "./components/ScrollReveal";
 
 const App = () => {
   return (
-    <div className="container mx-auto max-w-7xl">
+    <div className="container mx-auto max-w-7xl px-4">
       <Navbar />
       <Hero />
-      <About />
-      <Projects />
-      <Experiences />
-      <Testimonial />
-      <Contact />
-      <Footer/>
+      <ScrollReveal><About /></ScrollReveal>
+      <ScrollReveal delay={0.2}><Projects /></ScrollReveal>
+      <ScrollReveal delay={0.4}><Experiences /></ScrollReveal>
+      <ScrollReveal delay={0.6}><Testimonial /></ScrollReveal>
+      <ScrollReveal delay={0.8}><Contact /></ScrollReveal>
+      <Footer />
     </div>
   );
 };
