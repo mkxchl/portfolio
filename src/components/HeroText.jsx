c00ed7166c158502a4855c34402a1ee1436af118
Marchell Kevandra <117437628+mkxchl@users.noreply.github.com>
2026-01-05 12:48:05 +0700

    Shorten name in HeroText component

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -18,7 +18,7 @@ const HeroText = () => {
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hi I'm Marchell Kevandra
+          Hi I'm Marchell
         </motion.h1>
         <div className="flex flex-col items-start">
           <motion.p
@@ -61,7 +61,7 @@ const HeroText = () => {
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hi,I'm Marchell Kevandra
+          Hi,I'm Marchell
         </motion.p>
         <div>
           <motion.p
